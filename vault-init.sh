f593302b307dbb424bb57371ef340c95b6adfd87
Hamid <iamhamidteimouri@gmail.com>
2025-11-19 12:24:51 +0330

    Initial commit

--- vault-init.sh
+++ vault-init.sh
@@ -3,18 +3,22 @@
 set -ex
 
 unseal () {
-vault operator unseal $(grep 'Key 1:' /vault/file/keys | awk '{print $NF}')
-vault operator unseal $(grep 'Key 2:' /vault/file/keys | awk '{print $NF}')
-vault operator unseal $(grep 'Key 3:' /vault/file/keys | awk '{print $NF}')
+  vault operator unseal $(grep 'Key 1:' /vault/file/keys | awk '{print $NF}')
+  vault operator unseal $(grep 'Key 2:' /vault/file/keys | awk '{print $NF}')
+  vault operator unseal $(grep 'Key 3:' /vault/file/keys | awk '{print $NF}')
 }
 
 init () {
-vault operator init > /vault/file/keys
+   vault operator init > /vault/file/keys
 }
 
 log_in () {
    export ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/file/keys | awk '{print $NF}')
    vault login $ROOT_TOKEN
+}
+
+enable_approle () {
+   vault auth enable approle
 }
 
 create_token () {
@@ -28,6 +32,7 @@ else
    unseal
    log_in
    create_token
+   enable_approle
 fi
 
 vault status > /vault/file/status
